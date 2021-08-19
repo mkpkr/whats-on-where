704e692d8450d4bee37991b69874ce9b26aed4a1
Mike Parker <mikeparker1988@gmail.com>
2021-08-19 09:24:43 -0400

    Consolidated file parser classes

---
+++ src/main/java/com/mike/whatsonwhere/watchlist/csv/CsvWatchlistParser.java
@@ -0,0 +1,54 @@
+package com.mike.whatsonwhere.watchlist.csv;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+
+import com.mike.whatsonwhere.model.Movie;
+import com.mike.whatsonwhere.watchlist.WatchlistParser;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Profile("csv")
+public abstract class CsvWatchlistParser implements WatchlistParser {
+
+	@Value("${whatsonwhere.watchlist.path}")
+	private String watchlistPath;
+	
+	@Value("${whatsonwhere.watchlist.csv.movie-position}")
+	private int moviePosition;
+	
+	@Value("${whatsonwhere.watchlist.csv.year-position}")
+	private int yearPosition;
+	
+	public Queue<Movie> parseWatchlist() {
+		Queue<Movie> movies = new LinkedList<>();
+		try(Scanner scanner = new Scanner(new File(watchlistPath))) {
+			scanner.nextLine();
+			while(scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				movies.add(getMovieNameAndYear(line));
+			}
+			return movies;
+		} catch(FileNotFoundException e) {
+			log.error("File not found: " + watchlistPath, e);
+		} catch(Exception e) {
+			log.error("Exception while parsing watchlist file", e);
+		}
+		return new LinkedList<>();
+	}
+
+	private Movie getMovieNameAndYear(String line) {
+		String[] details = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+		return new Movie(details[moviePosition], details[yearPosition]);
+	}
+
+}
